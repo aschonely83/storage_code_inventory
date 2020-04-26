8584b1ae86ae2bf765d1b740eb3b5c2687041e6f
Aschonely <aschonely@gmail.com>
2020-04-26 15:01:57 -0400

    updated route name to reflect name change

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :customers
   resources :retailers
-  resources :users
+  resources :owners
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
