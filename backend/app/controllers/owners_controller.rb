de8fc2d09fd3703098bbb57770fdf5009b7d49b2
Aschonely <aschonely@gmail.com>
2020-04-27 16:35:26 -0400

    updated cors, owners controller, html and js file

--- backend/app/controllers/owners_controller.rb
+++ backend/app/controllers/owners_controller.rb
@@ -1,15 +1,18 @@
 class OwnersController < ApplicationController
   def index
     owners = Owner.all
-    render json: owners
+    render json: owners, except: [:created_at, :updated_at]
+  end
+
+  def show
+    owner = Owner.find_by_id(params[:id])
+    render json: owner, except: [:created_at, :updated_at]
   end
 
   def create
-    owner = Owner.create(owner_params)
+    owner = Owner.create(email: params[:email], password_digest: params[:password_digest])
     render json: owner
   end
 
-  def owner_params
-    params.require(:owner).permit(:email, :password_digest)
-  end
+ 
 end
---
+++ frontend/index.js
@@ -0,0 +1,3 @@
+const BASE_URL = "http://localhost:3000"
+const OWNERS_URL = `${BASE_URL}/owners`
+
