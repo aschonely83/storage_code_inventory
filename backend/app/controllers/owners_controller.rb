4d54150a2ccb0c583d1332979b38b7c6d819b0eb
Aschonely <aschonely@gmail.com>
2020-04-27 11:29:07 -0400

    made a create and strong params method for owners

--- backend/app/controllers/owners_controller.rb
+++ backend/app/controllers/owners_controller.rb
@@ -2,5 +2,14 @@ class OwnersController < ApplicationController
   def index
     owners = Owner.all
     render json: owners
-  end        
+  end
+
+  def create
+    owner = Owner.create(owner_params)
+    render json: owner
+  end
+
+  def owner_params
+    params.require(:owner).permit(:email, :password_digest)
+  end
 end
